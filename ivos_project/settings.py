063c2bccce8612bce7678c235beeec6b47a28a56
aldeniable <aldenrmd10@gmail.com>
2024-06-27 19:36:15 +0800

    cors

--- ivos_project/settings.py
+++ ivos_project/settings.py
@@ -40,11 +40,33 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware'
 ]
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # Replace with your React app's origin
+    'https://ivos-project-frontend.onrender.com',
+    "http://localhost:3000"  # Replace with your React app's origin
     # Add other allowed origins as needed
 ]
 CORS_ALLOW_ALL_HEADERS = True
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 ROOT_URLCONF = 'ivos_project.urls'
 
